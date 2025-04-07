7dc5d81be498dbcbc5111c2e530b1650c7b86124
livinglabsicets <livinglabsicets@gmail.com>
2025-04-07 16:43:08 +0530

    updated code for responsive for mobile and laptop screen

--- ogchoice/src/pages/Home.js
+++ ogchoice/src/pages/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './Home.css';
+import OgchoiceBeetrootImage from '../assets/images/OgchoiceBeetrootBlack.png';
 
-const Home = () => <div className="HomePage">
-
-<div class="Image">
-</div>
-
-</div>;
+const Home = () => (
+  <div className="HomePage">
+    <div className="Image">
+      <img src={OgchoiceBeetrootImage} alt="Example" />
+    </div>
+  </div>
+);
 
 export default Home;
